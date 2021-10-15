925ef4014f3d14feb5c4255437e18add7aa39598
ilajaait <ismael.lajaaiti@ens-lyon.fr>
2021-10-15 17:49:16 +0200

    Tune DNN (early stopping, dropout, increasing depth) Look at the convergence of R2 score for MLE predictions

--- neural-network-functions.R
+++ neural-network-functions.R
@@ -2,21 +2,23 @@ library(torch)
 library(ggplot2)
 
 
-get_loss_records <- function(fit_model, n_epochs){
+get_loss_records <- function(net.fit){
   names <- c("epochs", "loss", "type")
   df <- data.frame(matrix(nrow = 0, ncol = length(names)))
   colnames(df) <- names
-  for (i in 1:n_epochs){
-    train <- fit_model$records$metrics$train[[i]]$loss
-    valid <- fit_model$records$metrics$valid[[i]]$loss
+  n <- length(net.fit$records$metrics$train)  # number of epochs 
+  for (i in 1:n){
+    train <- net.fit$records$metrics$train[[i]]$loss
+    valid <- net.fit$records$metrics$valid[[i]]$loss
     df[nrow(df)+1, ] <- c(i, train, "train")
     df[nrow(df)+1, ] <- c(i, valid, "valid")
   }
   return(df)
 }
 
-plot_loss_records <- function(fit_model, n_epochs){
-  df_records <- get_loss_records(fitted2, n_epochs)
+
+plot_loss_records <- function(net.fit){
+  df_records <- get_loss_records(net.fit)
   
   df_records$epochs <- as.numeric(as.vector(df_records$epochs))
   df_records$loss <- as.numeric(as.vector(df_records$loss))
@@ -25,3 +27,6 @@ plot_loss_records <- function(fit_model, n_epochs){
     scale_y_continuous(trans='log10') +
     theme_minimal()
 }
+
+
+
