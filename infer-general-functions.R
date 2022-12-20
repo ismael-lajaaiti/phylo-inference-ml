9a0b321808d541905bfdf49b8e105d0105150ec9
Ismael Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-12-20 13:25:54 +0100

    Refactor `generate_ltt_dataframe`

--- infer-general-functions.R
+++ infer-general-functions.R
@@ -1156,30 +1156,23 @@ generatePhylo <- function(model, n_trees, n_taxa, param.range, ss_check = TRUE) 
 #'
 #' @export
 #' @examples
-generate_ltt_dataframe <- function(trees, n_taxa, true.param) {
-    n_trees <- length(trees) # number of trees
-    n_row <- ifelse(length(n_taxa) == 1, n_taxa, n_taxa[2])
-    df.ltt <- data.frame("tree1" = rep(NA, n_row))
-
-    df.rates <- as.data.frame(do.call(cbind, true.param))
-
-    cat("Creating LTT dataframe...\n")
-
-    for (i in 1:n_trees) {
-        tree <- trees[[i]] # get tree
-        ltt.coord <- ape::ltt.plot.coords(tree) # get ltt coordinates
-        ltt.coord <- as.data.frame(ltt.coord)
-        ltt.coord.time <- ltt.coord$time
-        n <- length(ltt.coord.time)
-        df.ltt[1:n, paste("tree", i, sep = "")] <- ltt.coord$time
-        progress(i, n_trees, progress.bar = TRUE, init = (i == 1))
+generate_ltt_dataframe <- function(tree_list,
+                                   range_tree_size,
+                                   verbose = TRUE) {
+    n_tree <- length(tree_list)
+    n_row <- max(range_tree_size)
+    df <- data.frame(matrix(nrow = n_row, ncol = 0))
+    for (i in 1:n_tree) {
+        tree <- tree_list[[i]]
+        ltt_temp <- ape::ltt.plot.coords(tree)
+        ltt_temp <- as.data.frame(ltt_temp)$time
+        n <- length(ltt_temp)
+        df[, i] <- c(ltt_temp, rep(1, n_row - n))
+        if (verbose) {
+            svMisc::progress(i, n_tree, progress.bar = TRUE, init = (i == 1))
+        }
     }
-
-    cat("\nCreating LTT dataframe... Done.")
-
-    out <- list("ltt" = df.ltt, "rates" = df.rates) # function output
-
-    return(out)
+    df
 }
 
 
