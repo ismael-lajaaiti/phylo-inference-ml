5a29a67d13f071a743e79a75913ce0068d4e5eea
Ismael Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-12-22 17:43:18 +0100

    Format `encode-cblv.R`

--- R/encode-cblv.R
+++ R/encode-cblv.R
@@ -1,6 +1,6 @@
 # This file contains the function to encode a phylogenetic tree with the
 # "Compact Bijective Ladderized Vector" methods
-# See Voznica 2021 - bioRxiv - DOI:10.1101/2021.03.11.435006 
+# See Voznica 2021 - bioRxiv - DOI:10.1101/2021.03.11.435006
 
 
 #### Libraries ####
@@ -25,20 +25,20 @@ library(torch)
 #' @param node integer, index of the node
 #' @param tree phylo tree
 #'
-#' @return logical 
+#' @return logical
 #' @export
 #' @examples
-is_tip <- function(node, tree){
-  n_tip <- length(tree$tip.label)
-  return(node <= n_tip)
-}
+# is_tip <- function(node, tree){
+#   n_tip <- length(tree$tip.label)
+#   return(node <= n_tip)
+# }
 
 
 #' Get the two children of a node
 #'
 #' The left children is always the node the further from the root
 #' The right children is always the node the closest to the root
-#' If the node is a tip returns NA values 
+#' If the node is a tip returns NA values
 #'
 #' @param node integer, index of the node
 #' @param tree phylo tree
@@ -49,17 +49,17 @@ is_tip <- function(node, tree){
 get_child <- function(node, tree){
   child <- list("left" = NA, "right" = NA)
   dist.all <- castor::get_all_distances_to_root(tree)
-  
+
   # First check that the node is not a tip (otherwise returns NA)
-  if (!is_tip(node, tree)){ 
+  if (!is_tip(node, tree)){
     idx <- phangorn::Children(tree, node)
-    
+
     # If both child are tips (same dist. to root), random attribution (l/r)
     if (is_tip(idx[1], tree) & is_tip(idx[2], tree)){
       child$left  <- idx[1]
       child$right <- idx[2]
     }
-    
+
     # Else (>= 1 child is internal), left child is the further from root
     else {
       dist.child <- c(dist.all[idx[1]], dist.all[idx[2]])
@@ -69,12 +69,12 @@ get_child <- function(node, tree){
       child$right <- idx[right]
     }
   }
-  return(child) 
+  return(child)
 }
 
 
-#' Traverse a tree inorder 
-#' 
+#' Traverse a tree inorder
+#'
 #'
 #' @param tree phylo tree
 #'
@@ -82,7 +82,7 @@ get_child <- function(node, tree){
 #' @export
 #' @examples
 traverse_inorder <- function(tree){
-  node  <- length(tree$tip.label) + 1 # root index 
+  node  <- length(tree$tip.label) + 1 # root index
   stack <- c()
   inorder <- c()
   while(length(stack) != 0 | !is.na(node)){
@@ -101,59 +101,59 @@ traverse_inorder <- function(tree){
 }
 
 
-#' Compute all the distances of tips to their most recent ancestor 
+#' Compute all the distances of tips to their most recent ancestor
 #'
-#' For each node, 
+#' For each node,
 #' 1. find its parent (most recent ancestor)
-#' 2. compute the distance between the node and its parent 
-#' This distance is only computed for tips, as we don't need this distance 
-#' for the internal nodes in the encoding. 
-#' Thus internal node indexes are filled with NA 
-#' See Voznica 2021 - bioRxiv - DOI:10.1101/2021.03.11.435006 
+#' 2. compute the distance between the node and its parent
+#' This distance is only computed for tips, as we don't need this distance
+#' for the internal nodes in the encoding.
+#' Thus internal node indexes are filled with NA
+#' See Voznica 2021 - bioRxiv - DOI:10.1101/2021.03.11.435006
 #'
 #' @param tree phylo tree
 #'
-#' @return vector of distance 
+#' @return vector of distance
 #' @export
 #' @examples
 get_all_distances_to_ancestor <- function(tree){
   n_node <- tree$Nnode # number of internal nodes
   n_tip  <- n_node + 1 # number of tips (size of the tree)
-  n_tot  <- n_tip + n_node # total number of nodes 
+  n_tot  <- n_tip + n_node # total number of nodes
   dist <- c()
-  
+
   # Fill tips value w/ their distance to their parent node
   for (tip in 1:n_tip){
     parent <- Ancestors(tree, tip, "parent") # get the parent of the tip
     bool   <- tree$edge[,1]==parent & tree$edge[,2]==tip # edge: parent ---> tip
-    edge.idx <- which(bool==TRUE) # edge index 
-    edge.length <- tree$edge.length[edge.idx] # edge length 
+    edge.idx <- which(bool==TRUE) # edge index
+    edge.length <- tree$edge.length[edge.idx] # edge length
     dist <- c(dist, edge.length) # save
   }
-  
+
   # Fill nodes values w/ NA
-  for (i in 1:n_node){ 
+  for (i in 1:n_node){
     dist <- c(dist, NA)
   }
   return(dist)
 }
 
-#' Encode a phylogenetic tree into a vector 
+#' Encode a phylogenetic tree into a vector
 #'
 #' Compute the compact encoding of a phylogenetic tree. This encoding is
-#' bijective. The encoding methods is named: "Compact Bijective Ladderized 
+#' bijective. The encoding methods is named: "Compact Bijective Ladderized
 #' Vector" (CBLV).
 #' See Voznica 2021 - bioRxiv - DOI:10.1101/2021.03.11.435006
 #'
-#' @param tree phylo tree to encode 
+#' @param tree phylo tree to encode
 #'
 #' @return vector containing the encoding
 #' @export
 #' @examples
 encode_phylo <- function(tree){
-  # 
-  # A list for nodes containing their distances to the root 
-  # A list for tips containing their distances to the most recent ancestor 
+  #
+  # A list for nodes containing their distances to the root
+  # A list for tips containing their distances to the most recent ancestor
   inorder <- traverse_inorder(tree)
   tips  <- c()
   nodes <- c()
@@ -195,12 +195,12 @@ encode_bisse <- function(tree){
 #' Format the encoding of a phylo tree
 #'
 #' Convert the encoding to a vector of dimension 1.
-#' The length of the vector is equal to the 2*N - 1 
+#' The length of the vector is equal to the 2*N - 1
 #' where N is the maximum possible number taxa
 #'
 #' @param tree.encode encoding of a phylo, generated by encode_phylo
-#' @param max_taxa maximum possible number of taxa, determines the length 
-#'                 of the returned torch tensor 
+#' @param max_taxa maximum possible number of taxa, determines the length
+#'                 of the returned torch tensor
 #'
 #' @return encode.vec, vector containing the encoding of the phylo tree
 #' @export
@@ -224,21 +224,21 @@ generate_encoding <- function(trees, n_taxa){
   list.encode <- list()
 
   cat("Computing encoding vectors...\n")
-  
+
   for (n in 1:n_trees){
     progress(n, n_trees, progress.bar = TRUE, init = (n==1))
     tree <- trees[[n]] # extract tree
     tree.encode   <- encode_phylo(tree) # encode the tree
     format.encode <- format_encode(tree.encode, max_taxa) # format the encoding
-    list.encode[[n]] <- format.encode # save to list 
+    list.encode[[n]] <- format.encode # save to list
   }
-  
-  # Convert the list of vectors to a torch tensor 
-  tensor.encode <- as.data.frame(do.call(cbind, list.encode)) %>% 
-    as.matrix() 
-  
+
+  # Convert the list of vectors to a torch tensor
+  tensor.encode <- as.data.frame(do.call(cbind, list.encode)) %>%
+    as.matrix()
+
   cat("\nComputing encoding vectors... Done.\n")
-  
+
   return(tensor.encode)
 }
 
@@ -249,21 +249,19 @@ generate_encoding_bisse <- function(trees, n_taxa){
   list.encode <- list()
 
   cat("Computing encoding vectors...\n")
-  
+
   for (n in 1:n_trees){
     progress(n, n_trees, progress.bar = TRUE, init = (n==1))
     tree <- trees[[n]] # extract tree
     tree.encode   <- encode_bisse(tree) # encode the tree
     format.encode <- format_encode(tree.encode, max_taxa) # format the encoding
-    list.encode[[n]] <- format.encode # save to list 
+    list.encode[[n]] <- format.encode # save to list
   }
-  
-  # Convert the list of vectors to a torch tensor 
-  matrix.encode <- as.data.frame(do.call(cbind, list.encode)) %>% 
-    as.matrix() 
-  
+
+  # Convert the list of vectors to a torch tensor
+  matrix.encode <- as.data.frame(do.call(cbind, list.encode)) %>%
+    as.matrix()
+
   cat("\nComputing encoding vectors... Done.\n")
   matrix.encode
 }
-
-
