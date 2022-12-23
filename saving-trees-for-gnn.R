0e63ca2492c395cdcdb002c662d48c22039b496a
Ismael Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-12-23 22:26:22 +0100

    Try to fix RAM issue while saving df for GNNs

--- saving-trees-for-gnn.R
+++ saving-trees-for-gnn.R
@@ -6,25 +6,30 @@ source("infer-general-functions.R")
 n_core <- 10
 n_rep <- 50
 prefix_raw_phylo <- "bisse-1e6-phylogenies/raw/bisse-n20000-trees-and-params"
-df_list <- mclapply(1:n_rep, function(i) {
-    out <- readRDS(paste(prefix_raw_phylo,
-        str_pad(i, 2, pad = "0"), ".rds",
-        sep = ""
-    ))
+prefix_gnn <- "bisse-1e6-phylogenies/formatted/graph/bisse-n20000-node-edge"
+mclapply(1:n_rep, function(i) {
+    out <- readRDS(
+        stringr::str_c(
+            prefix_raw_phylo,
+            stringr::str_pad(i, 2, pad = "0"),
+            ".rds"
+        )
+    )
     tree_list <- out$trees
     n_tree <- length(tree_list)
-    list_tree_df <- list()
+    df_list <- list()
     for (k in 1:n_tree) {
         tree <- tree_list[[k]]
         edge <- edge_df(tree)
         node <- node_df(tree)
         df <- list("node" = node, "edge" = edge)
-        list_tree_df[[k]] <- df
+        df_list[[k]] <- df
     }
-    list_tree_df
+    fname <- stringr::str_c(
+        prefix_gnn,
+        stringr::str_pad(i, 2, pad = "0"),
+        ".rds"
+    )
+    saveRDS(df_list, fname)
+    print(str_c(fname, " saved."))
 }, mc.cores = n_core)
-
-# [[1]]: [[1]] df1 [[2]] df2 -> [[1]] df1 [[2]] df2
-df_list_formatted <- unlist(df_list, recursive = FALSE)
-dir_gnn <- "bisse-1e6-phylogenies/formatted/graph/"
-saveRDS(df_list_formatted, str_c(dir_gnn, "bisse-node-edge-df-all.rds"))
